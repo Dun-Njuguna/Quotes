70c004d178f53bd35d7a37c756400dc25e46419c
Dun-Njuguna <duncankariuki60@gmail.com>
2019-01-14 13:38:50 +0300

    added delete functionality

--- src/app/quote-display/quote-display.component.ts
+++ src/app/quote-display/quote-display.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Output,EventEmitter,Input } from '@angular/core';
-import {Quotes} from '../quotes';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Quotes } from '../quotes';
 
 @Component({
   selector: 'app-quote-display',
@@ -10,32 +10,36 @@ import {Quotes} from '../quotes';
 
 
 export class QuoteDisplayComponent implements OnInit {
-    public count: number = 0;
-    public coun: number = 0;
-    userQuotes: any[];
-    constructor() {
-        this.userQuotes= [
-          new Quotes(1,"dun","Charlotte Brontë Jane Eyre","“I am no bird; and no net ensnares me: I am a free human being with an independent will.”"),
-          new Quotes(2,"duncan","Jane Eyre","ghbnjmk,., mknjbhvgfcvgbhnjmk,l.;,lmknj"),
-        ]
-    }
-    addQuote(newQuote) {
-      let userLength = this.userQuotes.length;
-      newQuote.id = userLength+1;
-      this.userQuotes.push(newQuote);
-      console.log(this.userQuotes);
-    }
+  userQuotes: any[];
+  @Output() isComplete = new EventEmitter<boolean>();
+  constructor() {
+    this.userQuotes = [
+      new Quotes(1, "dun", "Charlotte Brontë Jane Eyre", "“I am no bird; and no net ensnares me: I am a free human being with an independent will.”", 0, 0),
+      new Quotes(2, "duncan", "Jane Eyre", "ghbnjmk,., mknjbhvgfcvgbhnjmk,l.;,lmknj", 0, 0),
+    ]
+  }
+  addQuote(newQuote) {
+    let userLength = this.userQuotes.length;
+    newQuote.id = userLength + 1;
+    this.userQuotes.push(newQuote);
+    console.log(this.userQuotes);
+  }
 
-    toogleDetails(index){
-    this.userQuotes[index].showName = !this.userQuotes[index].showName;}
+  toogleDetails(index) {
+    this.userQuotes[index].showName = !this.userQuotes[index].showName;
+  }
 
-    upVote(i){
-        this.count += 1;
-    }
-    downVote(i){
-      this.coun -=1;
-    }
-    ngOnInit() {
+  goalDelete(i) {
+      this.userQuotes.splice(i, 1);
+  }
 
-    }
+  upVote(i) {
+    this.userQuotes[i].vote += 1;
+  }
+  downVote(i) {
+    this.userQuotes[i].dvote += 1;
+  }
+  ngOnInit() {
+
+  }
 }
--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,6 +1,6 @@
 export class Quotes {
       public showName:boolean;
-  constructor(public id:number, public name:string,public author:string,public quote:string){
+  constructor(public id:number, public name:string,public author:string,public quote:string, public vote:number, public dvote:number){
     this.showName=false;
   }
 }
