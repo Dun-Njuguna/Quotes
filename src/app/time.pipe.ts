28ab2916116e5a5ce11cf150b064d4bb1fe24243
Dun-Njuguna <duncankariuki60@gmail.com>
2019-01-14 04:34:41 +0300

    created functionality for the pipe

--- src/app/time.pipe.ts
+++ src/app/time.pipe.ts
@@ -6,7 +6,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TimePipe implements PipeTransform {
 
   transform(value: any): number {
+    let initial:Date =  new Date();
+    let todayTime: any =  new Date(initial.getTime());
+    let difference =Math.abs(value-todayTime )
 
+    let seconds = today*0.001;
+    var counter = seconds/86400;
+    // if (counter >= 1 && value > todayTime){
+    //      return counter;
+    //  }else{
+    //      return 0;
+    //  }
+    return counter;
   }
 
 }
